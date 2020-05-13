cdb44f7fa6e25a9958111ee5fd898544fe59c216
vaibhavc <vaibhavachandole9224@gmail.com>
2020-05-13 20:19:11 +0530

    map implemetation with location

---
+++ main.js
@@ -0,0 +1,197 @@
+let bounds;
+let infoWindow;
+let currentInfoWindow;
+let pos;
+let map;
+let service;
+let infoPane;
+let card;
+let input;
+let types;
+let strictBounds;
+var autocomplete;
+
+function initMap() {
+    // Initialize variables
+    bounds = new google.maps.LatLngBounds();
+    infoWindow = new google.maps.InfoWindow;
+    currentInfoWindow = infoWindow;
+    card = document.getElementById('pac-card');
+    input = document.getElementById('pac-input');
+    types = document.getElementById('type-selector');
+    strictBounds = document.getElementById('strict-bounds-selector');
+    autocomplete = new google.maps.places.Autocomplete(input);
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: pos,
+                zoom: 15
+            });
+
+            bounds.extend(pos);
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+
+            map.setCenter(pos);
+            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+            // Bind the map's bounds (viewport) property to the autocomplete object,
+            // so that the autocomplete requests use the current map bounds for the
+            // bounds option in the request.
+            autocomplete.bindTo('bounds', map);
+        }, () => {
+            // Browser supports geolocation, but user has denied permission
+            handleLocationError(true, infoWindow);
+        });
+
+        // Set the data fields to return when the user selects a place.
+        autocomplete.setFields(
+            ['address_components', 'geometry', 'icon', 'name']);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+            map: map,
+            anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function () {
+            infowindow.close();
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                // User entered the name of a Place that was not suggested and
+                // pressed the Enter key, or the Place Details request failed.
+                window.alert("No details available for input: '" + place.name + "'");
+                return;
+            }
+
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);  // Why 17? Because it looks good.
+            }
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+
+            var address = '';
+            if (place.address_components) {
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''),
+                    (place.address_components[1] && place.address_components[1].short_name || ''),
+                    (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+            }
+
+            infowindowContent.children['place-icon'].src = place.icon;
+            infowindowContent.children['place-name'].textContent = place.name;
+            infowindowContent.children['place-address'].textContent = address;
+            infowindow.open(map, marker);
+            let latt = place.geometry.location.lat();
+            let lonn = place.geometry.location.lng();
+            pos = {
+                lat: latt,
+                lng: lonn
+            };
+            // Call Places Nearby Search on user's location
+            getNearbyPlaces(pos);
+        });
+
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
+
+        document.getElementById('use-strict-bounds')
+            .addEventListener('click', function () {
+                console.log('Checkbox clicked! New state=' + this.checked);
+                autocomplete.setOptions({ strictBounds: this.checked });
+            });
+    } else {
+        // Browser doesn't support geolocation
+        handleLocationError(false, infoWindow);
+    }
+}
+
+// Handle a geolocation error
+function handleLocationError(browserHasGeolocation, infoWindow) {
+    // Set default location to Sydney, Australia
+    pos = { lat: -33.856, lng: 151.215 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 15
+    });
+
+    // Display an InfoWindow at the map center
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Geolocation permissions denied. Using default location.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+    currentInfoWindow = infoWindow;
+
+    /* TODO: Step 3B3, Call the Places Nearby Search */
+    // Call Places Nearby Search on the default location
+    getNearbyPlaces(pos);
+}
+/* END TODO: Step 2, Geolocate your user */
+/* TODO: Step 3B1, Call the Places Nearby Search */
+
+// Handle the results (up to 20) of the Nearby Search
+function nearbyCallback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        createMarkers(results);
+    }
+}
+
+/* TODO: Step 3C, Generate markers for search results */
+// Set markers at the location of each place result
+function createMarkers(places) {
+    places.forEach(place => {
+        let marker = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map,
+            title: place.name
+        });
+
+        /* TODO: Step 4B: Add click listeners to the markers */
+
+        // Adjust the map bounds to include the location of this marker
+        bounds.extend(place.geometry.location);
+    });
+    /* Once all the markers have been placed, adjust the bounds of the map to
+    * show all the markers within the visible area. */
+    map.fitBounds(bounds);
+}
+
+// Perform a Places Nearby Search Request
+function getNearbyPlaces(position) {
+    let request = {
+        location: position,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        keyword: 'hospital'
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, nearbyCallback);
+}
+
+// Sets a listener on a radio button to change the filter type on Places
+// Autocomplete.
+function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener('click', function () {
+        autocomplete.setTypes(types);
+    });
+}
